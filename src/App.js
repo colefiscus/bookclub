59e1157ee31e1fc8eeaee98c017fc6e6b591a796
colefiscus <>
2021-03-03 21:15:29 -0700

    Edit file structure and create Filter component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import getData from './util';
 import Selection from './Views/Selection/Selection';
 import Header from './Components/Header/Header';
 
-const App = () => {
-  return (
-    <>
-      <Header />
-      <Selection />
-    </>
-  )
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      lists: []
+    }
+  }
+
+  componentDidMount = () => {
+    return getData("https://api.nytimes.com/svc/books/v3/lists/names.json?api-key=obrhAVJmNNtUdhs3RSbGr7Shq6cwxtyH")
+      .then(data => this.setState({ lists: data.results }))
+  }
+
+  render() {
+    return (
+      <>
+        <Header />
+        <Selection lists={this.state.lists}/>
+      </>
+    )
+  }
 }
 
 export default App;
---
+++ src/Components/Filter/Filter.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Filter.css';
+
+class Filter extends Component {
+  constructor() {
+    super()
+    this.state = {
+      filteredLists: []
+    }
+  }
+}
+
+export default Filter;
