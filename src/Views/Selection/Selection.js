2a10c43e43303992817d360fc20f06a108718c0e
colefiscus <>
2021-03-03 19:34:49 -0700

    Add start to filter categories function

--- src/Views/Selection/Selection.js
+++ src/Views/Selection/Selection.js
@@ -11,6 +11,13 @@ class Selection extends Component {
     }
   }
 
+  filterBooks(event) {
+    const filteredBooks = this.state.list.filter(category => {
+      return category.display_name.includes(event.target.value)
+    })
+    this.setState({ list: filteredBooks })
+  }
+
   componentDidMount = () => {
     return getData("https://api.nytimes.com/svc/books/v3/lists/names.json?api-key=obrhAVJmNNtUdhs3RSbGr7Shq6cwxtyH")
       .then(data => this.setState({ list: data.results }))
@@ -22,20 +29,24 @@ class Selection extends Component {
         <h2 className="stepOne">Step 1: Pick a Category</h2>
         <form className="listFilters">
           <div>
-            <label for="kids">Kids</label>
-            <input name="kids" type="checkbox" />
+            <label htmlFor="all">All</label>
+            <input name="all" value="*" type="radio" onClick={(event) => this.filterBooks(event)} />
           </div>
           <div>
-            <label for="youngAdult">Young Adult</label>
-            <input name="youngAdult" type="checkbox" />
+            <label htmlFor="kids">Kids</label>
+            <input name="kids" value="Children" type="radio" onClick={(event) => this.filterBooks(event)} />
           </div>
           <div>
-            <label for="fiction">Fiction</label>
-            <input name="fiction" type="checkbox" />
+            <label htmlFor="youngAdult">Young Adult</label>
+            <input name="youngAdult" value="Young Adult" type="radio" onClick={(event) => this.filterBooks(event)} />
           </div>
           <div>
-            <label for="nonFiction">Non-Fiction</label>
-            <input name="nonFiction" type="checkbox" />
+            <label htmlFor="fiction">Fiction</label>
+            <input name="fiction" value="Fiction" type="radio" onClick={(event) => this.filterBooks(event)} />
+          </div>
+          <div>
+            <label htmlFor="nonFiction">Non-Fiction</label>
+            <input name="nonFiction" value="Nonfiction" type="radio" onClick={(event) => this.filterBooks(event)} />
           </div>
         </form>
         <ListBox list={this.state.list}/>
