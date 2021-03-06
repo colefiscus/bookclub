dc338c5e8e7faee05d51d809cdc5e50f0f5b7861
colefiscus <>
2021-03-06 13:29:46 -0700

    Delete unused fetch call and adjust code

--- src/App.js
+++ src/App.js
@@ -56,10 +56,9 @@ class App extends Component {
     getData(`https://openlibrary.org/isbn/${isbn}.json`)
       .then(data => {
         const apiData = getData(`https://openlibrary.org/api/books?bibkeys=ISBN:${isbn}&format=json&jscmd=data`)
-        const bookData = getData(`https://pacific-caverns-07550.herokuapp.com/https://openlibrary.org/${data.key}.json`)
         const workData = getData(`https://pacific-caverns-07550.herokuapp.com/https://openlibrary.org${data.works[0].key}.json`)
-        Promise.all([apiData, bookData, workData])
-          .then((details) => this.setState({ bookDetails: [details[0][`ISBN:${isbn}`], details[1], details[2]] }))
+        Promise.all([apiData, workData])
+          .then((details) => this.setState({ bookDetails: [details[0][`ISBN:${isbn}`], details[1]] }))
           .catch(error => console.log(error))
       })
   }
--- src/Views/BookInfo/BookInfo.js
+++ src/Views/BookInfo/BookInfo.js
@@ -8,12 +8,12 @@ const BookInfo = ({ currentBook, bookDetails }) => {
 
     let description;
 
-    if (!bookDetails[2].description) {
+    if (!bookDetails[1].description) {
       description = "No description available."
-    } else if (typeof bookDetails[2].description === 'string') {
-      description = bookDetails[2].description
-    } else if (bookDetails[2].description.value) {
-      description = bookDetails[2].description.value
+    } else if (typeof bookDetails[1].description === 'string') {
+      description = bookDetails[1].description
+    } else if (bookDetails[1].description.value) {
+      description = bookDetails[1].description.value
     }
 
     return (
@@ -31,7 +31,7 @@ const BookInfo = ({ currentBook, bookDetails }) => {
         <p>{bookDetails[0].number_of_pages}</p>
         <p>{bookDetails[0].publish_date}</p>
         {bookDetails[0].excerpts && <p>{bookDetails[0].excerpts[0].text}</p>}
-        {bookDetails[0].cover && <img src={bookDetails[0].cover.large} />}
+        {bookDetails[0].cover && <img src={bookDetails[0].cover.large} alt={`Cover of ${currentBook.book_details[0].title}`} />}
         <p>------------------ bookDetails[2] ---------------</p>
         <p>{description}</p>
       </main>
