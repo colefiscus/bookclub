4bccb82c80cc9164783d5ba28a1092e9ef504805
colefiscus <>
2021-03-07 22:19:07 -0700

    Add structure to Outcome for details/conditional

--- src/Views/Outcome/Outcome.js
+++ src/Views/Outcome/Outcome.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import './Outcome.css';
+import { Link } from 'react-router-dom';
 
 const Outcome = ({ matchingBooks }) => {
   if (matchingBooks.length) {
+
+    const books = matchingBooks.map(book => {
+      return (
+        <section key={book.book_details[0].primary_isbn13}>
+          <h2>{book.book_details[0].title}</h2>
+        </section>
+      )
+    })
+
     return (
-      <h2>yeehawwww we did it!</h2>
+      <main>
+        {books}
+      </main>
     )
   }
   else {
-    <h2>Dang none of those books matched! Try again?</h2>
+    return (
+      <main>
+        <h2>Dang none of those books matched! Try again?</h2>
+        <Link to="/">Back to Home</Link>
+      </main>
+    )
   }
 }
 
