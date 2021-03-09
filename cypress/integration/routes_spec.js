7dbd8fcc9f859a0684d45797f40b566d005279b3
colefiscus <>
2021-03-08 21:00:33 -0700

    Add test for category preview url

--- cypress/integration/routes_spec.js
+++ cypress/integration/routes_spec.js
@@ -1,9 +1,17 @@
 describe('Routes', () => {
   beforeEach(() => {
-    cy.visit('localhost:3000')
+    // cy.visit('localhost:3000').wait(1000)
   })
 
-  it.only('Should load on the home page', () => {
-    cy.url().should("include", "3000/")
+  it('Should load on the home page', () => {
+    cy.url().should('include', '3000/')
   })
+
+  it.only('Should show the preview URL when clicking on a category', () => {
+    cy
+      .get('article:first').children('a:first').click()
+      .url().should('include', 'preview/combined-print-and-e-book-fiction')
+  })
+
+  it
 })
