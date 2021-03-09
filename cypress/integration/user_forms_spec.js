8780a896c7c1a2ec6ae7d37bbb87901214656805
colefiscus <>
2021-03-08 19:54:12 -0700

    Add test to check that book details can be viewed

--- cypress/integration/user_forms_spec.js
+++ cypress/integration/user_forms_spec.js
@@ -1,7 +1,7 @@
 describe('User Selection and Book Details', () => {
   beforeEach(() => {
     cy
-      // .visit('http://localhost:3000').wait(1500)
+      .visit('http://localhost:3000').wait(1500)
       .get('article').eq(2).children('a:last').click()
   })
 
@@ -20,7 +20,7 @@ describe('User Selection and Book Details', () => {
     cy
       .get('input[class=nameInput]:first').type('Bernie')
       .get('input[class=nameInput]').eq(1).type('Barrack')
-      .get('button').click()
+      .get('button').click().wait(7000)
       .get('form').children('div').should('have.length', 3)
   })
 
@@ -38,7 +38,7 @@ describe('User Selection and Book Details', () => {
       .get('section:last').children('h2').should('contain', 'Elizabeth')
   })
 
-  it.only('Should show a list of books with a preview link to show details', () => {
+  it('Should show a list of books with a preview link to show details', () => {
     cy
       .get('input').type(3)
       .get('button').click()
@@ -50,5 +50,23 @@ describe('User Selection and Book Details', () => {
     cy
       .get('section:first').children('div[class=bookVote]').should('have.length', 10)
       .get('div[class=bookVote]').eq(2).children('button').should('contain', 'THE MIDNIGHT LIBRARY')
+  })
+
+  it.only('Should show extended book details after clicking a preview link', () => {
+    cy
+      .get('input').type(3)
+      .get('button').click()
+    cy
+      .get('input[class=nameInput]:first').type('Bernie')
+      .get('input[class=nameInput]').eq(1).type('Barrack')
+      .get('input[class=nameInput]:last').type('Elizabeth')
+      .get('button').click()
+    cy
+      .get('div[class=bookVote]').eq(2).children('a').click()
+      .get('h2:first').should('contain', 'THE MIDNIGHT LIBRARY')
+      .get('img').should('have.attr', 'src', 'https://covers.openlibrary.org/b/id/10313767-L.jpg')
+    cy
+      .get('a:first').click()
+      .get('section:first').children('h2').should('contain', 'Bernie')
   })
 })
