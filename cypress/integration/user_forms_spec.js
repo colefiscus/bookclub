72d285ed7b38050a1d5182a17b8b7e5bfb0be4db
colefiscus <>
2021-03-08 18:39:45 -0700

    Add tests to add number of users + names

--- cypress/integration/user_forms_spec.js
+++ cypress/integration/user_forms_spec.js
@@ -1,6 +1,27 @@
 describe('User Selection and Book Details', () => {
   beforeEach(() => {
     cy
-      
+      // .visit('http://localhost:3000').wait(1500)
+      .get('article').eq(2).children('a:last').click()
+  })
+
+  it('Should create users based on user input', () => {
+    cy
+      .get('input').type(3)
+      .get('button').click()
+      .get('form').children('div').should('have.length', 3)
+  })
+
+  it('Should give the users names', () => {
+    cy
+      .get('input').type(3)
+      .get('button').click()
+      .get('form').children('div').should('have.length', 3)
+    cy
+      .get('input[class=nameInput]:first').type('Bernie')
+      .get('input[class=nameInput]').eq(1).type('Barrack')
+      .get('button').click()
+      .get('input[class=nameInput]:last').type('Elizabeth')
+      .get('button').click()
   })
 })
