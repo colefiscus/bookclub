cea929eaa41c3f8f8f5a49d31ca312c5a53ec85c
colefiscus <>
2021-03-08 22:42:33 -0700

    Add to response for first GET and make assertion

--- cypress/integration/network_and_error_spec.js
+++ cypress/integration/network_and_error_spec.js
@@ -1,10 +1,17 @@
 describe('Network Requests and Error Handling', () => {
   beforeEach(() => {
     cy.fixture('mock_categories')
-      .then(() => {
-        cy.intercept('https://api.nytimes.com/svc/books/v3/lists/names.json?api-key=obrhAVJmNNtUdhs3RSbGr7Shq6cwxtyH')
+      .then((categories) => {
+        cy.intercept('https://api.nytimes.com/svc/books/v3/lists/names.json?api-key=obrhAVJmNNtUdhs3RSbGr7Shq6cwxtyH', {
+          status: 201,
+          body: categories
+        })
       })
     cy
     .visit('http://localhost:3000').wait(1500)
+  })
+
+  it('Should retrieve categories on page load', () => {
+    cy.get('a:first').should('contain', 'Super Magical Action Fiction')
   })
 })
