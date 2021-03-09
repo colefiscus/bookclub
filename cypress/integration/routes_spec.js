e98e6d46097398783b299a9bdb2b0e9bc10e0f25
colefiscus <>
2021-03-08 21:12:41 -0700

    Add test that URL goes home after matching books

--- cypress/integration/routes_spec.js
+++ cypress/integration/routes_spec.js
@@ -1,6 +1,6 @@
 describe('Routes', () => {
   beforeEach(() => {
-    // cy.visit('localhost:3000').wait(1000)
+    cy.visit('localhost:3000').wait(1000)
   })
 
   it('Should load on the home page', () => {
@@ -17,5 +17,45 @@ describe('Routes', () => {
     cy
       .get('article:first').children('a:last').click()
       .url().should('include', '/approval')
+  })
+
+  it('Should show the book title URL when selecting specific book for more details', () => {
+    cy
+      .get('article:first').children('a:last').click()
+      .get('input').type(3)
+      .get('button').click()
+    cy
+      .get('input[class=nameInput]:first').type('Bernie')
+      .get('input[class=nameInput]').eq(1).type('Barrack')
+      .get('input[class=nameInput]:last').type('Elizabeth')
+      .get('button').click()
+    cy
+      .get('section:first').children('div[class=bookVote]').eq(5).children('a:last').click()
+      .url().should('include', '/details/THE%20MIDNIGHT%20LIBRARY')
+  })
+
+  it.only('Should return back to the home URL when completing a vote', () => {
+    cy
+      .get('article:first').children('a:last').click()
+      .get('input').type(3)
+      .get('button').click()
+    cy
+      .get('input[class=nameInput]:first').type('Bernie')
+      .get('input[class=nameInput]').eq(1).type('Barrack')
+      .get('input[class=nameInput]:last').type('Elizabeth')
+      .get('button').click()
+    cy
+      .get('section:first').children('div[class=bookVote]').eq(0).click()
+      .get('button[class=voteCompleteButton]:first').click()
+    cy
+      .get('section').eq(1).children('div[class=bookVote]').eq(0).click()
+      .get('button[class=voteCompleteButton]').eq(1).click()
+    cy
+      .get('section:last').children('div[class=bookVote]').eq(0).click()
+      .get('button[class=voteCompleteButton]:last').click()
+    cy
+      .get('button[class=finalSubmitButton]').click()
+      .get('a').click()
+      .url().should('include', '3000/')    
   })
 })
